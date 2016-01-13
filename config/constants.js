a8538f1634f8dc7d244be6773133744c7228e031
sankarvema <sans.vema@gmail.com>
2016-01-13 12:53:33 +0530

    Constants added

---
+++ config/constants.js
@@ -0,0 +1,14 @@
+// defines application level constants
+//    donot change
+
+module.exports = {
+
+    'paths' : {						// constants defining the application paths
+        'routes'        	: __dirname + '/routers',
+        'views'    				: __dirname + '/views',
+        'models'     			: __dirname + '/models',
+        'app'							: __dirname + '/app',
+        'api'							: __dirname + '/api'
+    },
+
+};
