91a76c292dc4861b2eb8b03e01479dfed08d9c20
sankarvema <sans.vema@gmail.com>
2016-01-19 11:34:27 +0530

    Route modification added for token

--- app/routes.js
+++ app/routes.js
@@ -203,5 +203,12 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
 
-    res.redirect('/');
+    console.log('token index ' + (req.url.indexOf('/token')));
+
+    // A simple detour for token access
+    // Application can access token after login to connect with API
+    if(req.url.indexOf('/token') > -1)
+        res.status(404).send("Not Found");
+    else
+        res.redirect('/');
 }
