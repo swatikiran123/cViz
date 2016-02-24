9abe9bab64e6caa95852d94c6baea8199a549fd5
sankarvema <sans.vema@gmail.com>
2016-02-24 14:51:43 +0530

    User picker directive working
    Pending areas
    - Hide & display text box dynamically

--- public/mods/directives/user/script.js
+++ public/mods/directives/user/script.js
@@ -6,26 +6,38 @@ angular.module('userDirective', [])
     input: '',
     id: '',
     user: {},
-    output:'',
-    found:'false'
+    output:''
+    //found:'false'
   };
 
-//var $scope.show=false;
+//$scope.showUser = false;
+  $scope.showFlag = "none";
 
   $scope.getUser = function(){
-    console.log($scope.data.input);
+    //console.log($scope.data.input);
 
     $http.get('/api/v1/admin/users/' + $scope.data.input).success(function(response) {
-      console.log(response);
+      //console.log(response);
       $scope.data.user = response;
-      $scope.data.found = 'true';
-$scope.show = true;
-      $scope.data.output = parse("%s %s, <%s>", $scope.data.user.name.first, 
-        $scope.data.user.name.last, $scope.data.user.email);
-      console.log($scope.data);
+      //$scope.data.found = 'true';
+
+      $scope.showFlag = "user";
+/*      $scope.data.output = parse("%s %s, <%s>", $scope.data.user.name.first, 
+        $scope.data.user.name.last, $scope.data.user.email);*/
+      //console.log($scope.data);
     })
-    .error(function(data, status){
-      console.log("error with user directive");
+    .error(function(response, status){
+/*      console.log(response.status);
+      console.log(status);*/
+      //$scope.showUser = false;
+      $scope.showFlag = "noUser";
+      if(status===404)
+      {
+        //console.log("User not found");
+        message = "User not found";
+      }
+      else
+        console.log("error with user directive");
     });
   }
   
@@ -34,12 +46,7 @@ $scope.show = true;
 .directive('user', function() {
   return {
     controller: 'userDirectiveControllerMain',
-/*    templateUrl: function(elem, attr){
-      return 'user-'+attr.type+'.html';
-    }*/
     templateUrl: '/public/mods/directives/user/templates/user-picker.html'
-    //templateUrl: 'user-picker.html'
-    //template: 'Name: {{user.name}} Address: {{user.address}}'
   };
 });
 
