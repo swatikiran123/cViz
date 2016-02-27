374a50e07cc8fff0049d36440054a27b52b69e00
sankarvema <sans.vema@gmail.com>
2016-02-27 13:45:42 +0530

    Refactor mobile layout
    - Mobile layout refactored with header
    - Periodic housekeeping of code
    - Datepicker module definition issue
    - live clock removed
    - mobile application pages added
    - Some styling issues addressed

--- routes/mobileRoutes.js
+++ routes/mobileRoutes.js
@@ -3,24 +3,31 @@ var auth = require('./auth.js');
 module.exports = function(app, passport) {
 
     // show welcome screen
-    app.get('/m/welcome', auth.isLoggedIn, function(req, res) {
-        res.locals.pageTitle = "Welcome to CSC";
+    app.get('/m/welcome', function(req, res) {
+        res.locals.pageTitle = "Welcome...";
         res.render('mobile/welcome.ejs', {
             layout: 'layouts/mobile'
         });
     });
-    app.get('/m/session', auth.isLoggedIn, function(req, res) {
+    
+    app.get('/m/sessions', function(req, res) {
         res.locals.pageTitle = "Visit schedules";
-        res.render('mobile/session.ejs', {
-            layout: 'layouts/schedule'
+        res.render('mobile/sessions.ejs', {
+            layout: 'layouts/mobile'
         });
     });
 
+    app.get('/m/session', function(req, res) {
+        res.locals.pageTitle = "Session";
+        res.render('mobile/session.ejs', {
+            layout: 'layouts/mobile'
+        });
+    });
 
     // show csclocations screen
-    app.get('/m/csclocations', auth.isLoggedIn, function(req, res) {
-        res.locals.pageTitle = "Go your own way";
-        res.render('mobile/csclocation.ejs', {
+    app.get('/m/factsheet', function(req, res) {
+        res.locals.pageTitle = "Our fact sheet";
+        res.render('mobile/factsheet.ejs', {
             layout: 'layouts/mobile'
         });
     });
