504b84ea8b88d183b08c4effe0f6711def37ee47
sankarvema <sans.vema@gmail.com>
2016-02-29 09:55:33 +0530

    Mobile angular template added

--- routes/mobileRoutes.js
+++ routes/mobileRoutes.js
@@ -2,25 +2,27 @@ var auth = require('./auth.js');
 
 module.exports = function(app, passport) {
 
+    var activeLayout = 'layouts/mobile';
+
     // show welcome screen
     app.get('/m/', function(req, res) {
         res.locals.pageTitle = "Welcome...";
         res.render('mobile/welcome.ejs', {
-            layout: 'layouts/mobile'
+            layout: activeLayout
         });
     });
-    
+
     app.get('/m/sessions', function(req, res) {
         res.locals.pageTitle = "Visit schedules";
         res.render('mobile/sessions.ejs', {
-            layout: 'layouts/mobile'
+            layout: activeLayout
         });
     });
 
     app.get('/m/session', function(req, res) {
         res.locals.pageTitle = "Session";
         res.render('mobile/session.ejs', {
-            layout: 'layouts/mobile'
+            layout: activeLayout
         });
     });
 
@@ -28,7 +30,7 @@ module.exports = function(app, passport) {
     app.get('/m/factsheet', function(req, res) {
         res.locals.pageTitle = "Our fact sheet";
         res.render('mobile/factsheet.ejs', {
-            layout: 'layouts/mobile'
+            layout: activeLayout
         });
     });
 
