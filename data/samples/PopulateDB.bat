df40387230698b8b051d18bbe49639bb7c28a45b
sankarvema <sans.vema@gmail.com>
2016-03-01 22:44:44 +0530

    Sample data tuned for reference fields

--- data/samples/PopulateDB.bat
+++ data/samples/PopulateDB.bat
@@ -6,19 +6,19 @@ ECHO:
 ECHO Creating User Collection
 mongoimport --db cViz-Test --collection users --file Json's\user.json --type json --jsonArray
 
-
+pause
 
 ECHO:
 ECHO Creating Client Collection
 mongoimport --db cViz-Test --collection clients --file Json's\client.json --type json --jsonArray
 
-
+pause
 
 ECHO:
 ECHO Creating Visit Collection
 mongoimport --db cViz-Test --collection visits --file Json's\visit.json --type json --jsonArray
 
-
+pause
 
 ECHO:
 ECHO Creating Visit_schedule Collection
