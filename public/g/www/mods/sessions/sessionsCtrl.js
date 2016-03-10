e92c5793de3bb1177bd57380e77d97a87bfcac8c
mnimmagari <madhumithareddy185@gmail.com>
2016-03-10 19:49:24 +0530

    Mobile Session data retrieving

--- public/g/www/mods/sessions/sessionsCtrl.js
+++ public/g/www/mods/sessions/sessionsCtrl.js
@@ -1,18 +1,14 @@
 angular.module('sessions', [])
 
-.controller('sessionsCtrl', function($scope) {
-
-  $scope.sessions = [
-    { title: 'Welcome note by Managing Director', id: 1, startTime: '08:30', endTime: '09:00' },
-    { title: 'Keynote session from Chief of Operations', id: 2, startTime: '09:00', endTime: '09:30' },
-    { title: 'Big Data Session', id: 3, startTime: '09:30', endTime: '10:30' },
-    { title: 'Tea Break', id: 4, startTime: '10:30', endTime: '11:30' },
-    { title: 'Innovation Session', id: 5, startTime: '11:30', endTime: '12:30' },
-    { title: 'Lunch', id: 6, startTime: '12:30', endTime: '13:30' }
-  ];
+.controller('sessionsCtrl', function($scope, $http) {
+    $http.get('/api/v1/secure/visitSchedules').success(function(response) {
+        $scope.visitScheduleList = response;
+    });
 })
 
-.controller('sessionCtrl', function($scope, $stateParams) {
-	console.log("session controller running");
-	$scope.session = { title: 'Welcome note by Managing Director', id: 1, startTime: '08:30', endTime: '09:00' };
+.controller('sessionCtrl', function($scope, $routeParams, $http) {
+    console.log("session controller running");
+    $http.get('/api/v1/secure/visitSchedules/' + $routeParams.id).success(function(response) {
+        $scope.visitSchedule = response;
+    });
 });
