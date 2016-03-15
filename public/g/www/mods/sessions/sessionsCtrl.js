5a3b3107f9f1e8d4b4fd7082d21b502ecb66c007
swatikiran123 <swakiri125@gmail.com>
2016-03-15 18:38:26 +0530

    visit planner- added visiotor directive picker. issue #81 pipeline-5 also added seccionCtrl.

---
+++ public/g/www/mods/sessions/sessionsCtrl.js
@@ -0,0 +1,14 @@
+angular.module('sessions', [])
+
+.controller('sessionsCtrl', function($scope, $routeParams, $http) {
+    $http.get('/api/v1/secure/visits/' + $routeParams.id + '/sessions').success(function(response) {
+        $scope.scheduleList = response;
+    });
+})
+
+.controller('sessionCtrl', function($scope, $routeParams, $http) {
+    console.log("session controller running");
+    $http.get('/api/v1/secure/visitSchedules/' + $routeParams.id).success(function(response) {
+        $scope.session = response;
+    });
+});
