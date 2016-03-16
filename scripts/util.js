4ce5b4eba518252b6939f7de735122a4350e58e0
sankarvema <sans.vema@gmail.com>
2016-03-16 17:48:50 +0530

    String methods added to utils

--- scripts/util.js
+++ scripts/util.js
@@ -1,10 +1,11 @@
+var colors				= require('colors');
+
 var util = {};
 
 util.formatString = formatString;
 util.dateOnly = dateOnly;
 
 module.exports = util;
-
 
 function formatString(str) {
   var args = [].slice.call(arguments, 1),
@@ -15,7 +16,20 @@ function formatString(str) {
   });
 };
 
-
 function dateOnly(myDate){
 	return (new Date(myDate.getFullYear(), myDate.getMonth(), myDate.getDate()));
 };
+
+// string extender methods
+
+String.prototype.repeat= function(n){
+    n= n || 1;
+    return Array(n+1).join(this);
+}
+
+String.prototype.compare = function(str){
+	if(typeof str === 'object')
+		str = "" + str;
+
+	return this.trim().toLowerCase() == str.trim().toLowerCase();
+}
