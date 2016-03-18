c2c88dfe287538f47fc834b9303393c0d64500ef
swatikiran123 <swakiri125@gmail.com>
2016-03-18 12:07:10 +0530

    Extended visit model with fields billable,chargecode,wbscode.

--- models/visit.js
+++ models/visit.js
@@ -9,33 +9,36 @@ var clientSchema 			= require('./client');
 
 var visitSchema = new mongoose.Schema({
 
-	title						    : { type: String },
-	client 						    : { type: Schema.Types.ObjectId, ref: 'clients' },//{ type: String },
-	agenda							: { type: String },
-	agm								: { type: Schema.Types.ObjectId, ref: 'User' },
-	anchor							: { type: Schema.Types.ObjectId, ref: 'User' },
+	title						    : { type: String, required: true },
+	client 						    : { type: Schema.Types.ObjectId, ref: 'clients', required: true },//{ type: String },
+	agenda							: { type: String, required: true },
+	agm								: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	anchor							: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 	schedule						: [{
-		startDate					: { type: Date},//, default: Date.now },
-		endDate						: { type: Date},//, default: Date.now },
-		location					: { type: String }  // set of csc locations
+		startDate					: { type: Date, required: true},
+		endDate						: { type: Date, required: true},
+		location					: { type: String, required: true }  // set of csc locations
 	}],
+	billable						: { type: String, lowercase: true, trim: true, required: true, enum: ['billable', 'non-billable']},
+	wbsCode							: { type: String, trim: true },
+	chargeCode						: { type: String, trim: true },
 	visitors						: [{
-		visitor						: { type: Schema.Types.ObjectId, ref: 'User' },
-		influence					: { type: String, lowercase: true, trim: true },		// {Decision Maker, Influencer, End User, Others}
+		visitor						: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+		influence					: { type: String, lowercase: true, trim: true, required: true },		// {Decision Maker, Influencer, End User, Others}
 	}],
 	interest						: {
-		businessType			    : { type: String, lowercase: true, trim: true },		// {new, repeat}
-		visitType					: { type: String, lowercase: true, trim: true },		// {new, repeat}
-		objective					: { type: String }
+		businessType			    : { type: String, lowercase: true, trim: true, required: true },		// {new, repeat}
+		visitType					: { type: String, lowercase: true, trim: true, required: true },		// {new, repeat}
+		objective					: { type: String, required: true }
 	},
-	status							: { type: String, lowercase: true, trim: true },		// {confirmed, tentative, freeze, done}
-	createBy						: { type: Schema.Types.ObjectId, ref: 'User' },
-	createOn						: { type: Date, default: Date.now },
-	feedbackTmpl				    : { type: Schema.Types.ObjectId, ref: 'feedbackDefs' },
+	status							: { type: String, lowercase: true, trim: true, required: true },		// {confirmed, tentative, freeze, done}
+	createBy						: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	createOn						: { type: Date, default: Date.now, required: true },
+	feedbackTmpl				    : { type: Schema.Types.ObjectId, ref: 'feedbackDefs', required: true },
 	 keynote						: [{
-		note   						: { type: Schema.Types.ObjectId, ref: 'keynotes' },
-	 	context 					: {type: String, enum: ['welcome', 'thankyou']},
-	 	order						: {type: Number}
+		note   						: { type: Schema.Types.ObjectId, ref: 'keynotes', required: true },
+	 	context 					: {type: String, enum: ['welcome', 'thankyou'], required: true},
+	 	order						: {type: Number, required: true}
 	 }] 
 
 });
