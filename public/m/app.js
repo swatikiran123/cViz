b1871ef507075d75ad8a64d9508199718889efbe
swatikiran123 <swakiri125@gmail.com>
2016-03-24 12:12:51 +0530

    Additional changes styles in client,contactlist modules.

--- public/m/app.js
+++ public/m/app.js
@@ -1,8 +1,6 @@
 'use strict';
 
-<<<<<<< HEAD
-
 angular.module('mviz-main', ['home','locator','execBios']);
-angular.module('mviz-visits', ['ui.bootstrap','visits','sessions','contacts','userdisplayDirective']);
+angular.module('mviz-visits', ['ui.bootstrap','visits','sessions','contacts','userDirective']);
 angular.module('mviz-facts', ['ui.bootstrap','facts','lctnGalry']);
 angular.module('mviz-emp', []);
--- public/m/contacts/contactsCtrl.js
+++ public/m/contacts/contactsCtrl.js
@@ -6,7 +6,7 @@ angular.module('contacts')
 	$scope.small= "small";
 	$scope.large= "LARGE";
 	$scope.medium= "medium";
-	$http.get('/api/v1/secure/contactList/city/' + $routeParams.city).success(function(response) {//add location here after /city/
+	$http.get('/api/v1/secure/contactList/city/' + $routeParams.city).success(function(response) {
 		$scope.contactList = response;
 
 	})
