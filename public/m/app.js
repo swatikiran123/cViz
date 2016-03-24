48bb3d46291020b8ab4b96495a7c319c206e18a4
swatikiran123 <swakiri125@gmail.com>
2016-03-24 14:40:52 +0530

    Mobile part public/m/app.js added userdisplaydirective.

--- public/m/app.js
+++ public/m/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
 angular.module('mviz-main', ['home','locator','execBios']);
-angular.module('mviz-visits', ['ui.bootstrap','visits','sessions','contacts','userDirective']);
+angular.module('mviz-visits', ['ui.bootstrap','visits','sessions','contacts','userdisplayDirective']);
 angular.module('mviz-facts', ['ui.bootstrap','facts','lctnGalry']);
 angular.module('mviz-emp', []);
