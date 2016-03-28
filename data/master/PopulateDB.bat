866c6559a19ca579ac384c36006dd639d21b271d
sankarvema <sans.vema@gmail.com>
2016-03-28 13:52:28 +0530

    data folder refresh

---
+++ data/master/PopulateDB.bat
@@ -0,0 +1,20 @@
+ECHO OFF
+ECHO Initializing cViz-Test DB with master data...
+
+ECHO:
+ECHO "Importing master user..."
+mongoimport --db cViz-Test --collection users --file jsonFiles\user.json --type json --jsonArray
+
+
+ECHO:
+ECHO "Importing master group structure..."
+mongoimport --db cViz-Test --collection groups --file jsonFiles\group.json --type json --jsonArray
+
+
+ECHO:
+ECHO "Importing master List of Values..."
+mongoimport --db cViz-Test --collection lovs --file jsonFiles\lov.json --type json --jsonArray
+
+
+ECHO:
+PAUSE
