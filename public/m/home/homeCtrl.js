7a903c18da31ee31faad4686b2e8c17d28dc5af1
Sankar Vema <sans.vema@gmail.com>
2016-03-29 06:58:56 +0000

    schedule styles applied

--- public/m/home/homeCtrl.js
+++ public/m/home/homeCtrl.js
@@ -5,3 +5,21 @@ var app = angular.module('home');
 app.controller('homeCtrl', ['$scope', 'location', function ($scope, location) {
    location.get(angular.noop, angular.noop);
 }]);
+
+app.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+			console.log("scrolling...");
+    	 	scope.navClass = 'big';
+        //scope.data = false;
+        angular.element($window).bind("scroll", function() {
+             if (this.pageYOffset >= 100) {
+                 scope.navClass = 'small';
+                 //scope.data = true;
+             } else {
+                  scope.navClass = 'big';
+                  //scope.data = false;
+             }
+            scope.$apply();
+        });
+    };
+});
