bd2afd337fffa294df13ebcfb16a748f0d6e314e
agupta330 <agupta330@csc.com>
2016-03-31 15:04:55 +0530

    Module Feedback Directive
    -added service,models,apiroutes for feedbacks
    -created feedback directive which show the data from feedbackDefs based on the feedback id.
    -added appAssets for feedback Directive
    -included the feedback view mode(byperson or byquestion) in visitGrid.html.
    -changes done in add feedbackDefs template to add items in dialog box.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,7 +21,7 @@ var cityFacts				= require(constants.paths.controllers + '/api/cityFacts');
 var factSheets				=	require(constants.paths.controllers + '/api/factSheets');
 var teasers					=	require(constants.paths.controllers + '/api/teasers');
 var contactList			=	require(constants.paths.controllers + '/api/contactList');
-
+var feedbacks				=	require(constants.paths.controllers + '/api/feedbacks');
 
 /*
  * Routes that can be accessed by any one
@@ -101,7 +101,7 @@ router.delete('/api/v1/secure/visitSchedules/:id', visitSchedule.deleteById);
 
 // List of service routes for feedbackDefs
 router.get('/api/v1/secure/feedbackDefs', feedbackDef.getAll);
-//router.get('/api/v1/secure/feedbackDefs/:id', feedbackDef.getOneById);
+router.get('/api/v1/secure/feedbackDefs/id/:id', feedbackDef.getOneById);
 router.post('/api/v1/secure/feedbackDefs', feedbackDef.create);
 router.put('/api/v1/secure/feedbackDefs/:id', feedbackDef.updateById);
 router.delete('/api/v1/secure/feedbackDefs/:id', feedbackDef.deleteById);
@@ -143,5 +143,12 @@ router.get('/api/v1/secure/admin/groups/:id', groups.getOneById);
 router.post('/api/v1/secure/admin/groups', groups.create);
 router.put('/api/v1/secure/admin/groups/:id', groups.updateById);
 router.delete('/api/v1/secure/admin/groups/:id', groups.deleteById);
+
+// List of service routes for feedbacks
+router.get('/api/v1/secure/feedbacks', feedbacks.getAll);
+router.get('/api/v1/secure/feedbacks/:id', feedbacks.getOneById);
+router.post('/api/v1/secure/feedbacks', feedbacks.create);
+router.put('/api/v1/secure/feedbacks/:id', feedbacks.updateById);
+router.delete('/api/v1/secure/feedbacks/:id', feedbacks.deleteById);
 
 module.exports = router;
