e3d65c1992b9bdcb003c90d3da63942e44d30125
mnimmagari <madhumithareddy185@gmail.com>
2016-04-07 12:03:00 +0530

    1. css files names changed 2. Myvisit page search bar functionality added 3. ExecBios search functionality for cscRepresentatives added

--- public/d/header/header.js
+++ public/d/header/header.js
@@ -5,16 +5,20 @@ angular.module('header', []).directive("header", function() {
       showprofile:'@',
       icon:'@',
       title1:'@',
-      title2:'@' 
-    },
-
+      title2:'@',
+      query:'='
+          },
+          restrict:'EA',
      transclude: true,
     templateUrl: "/public/d/header/header.html",
 
-     link : function(scope,element,attrs)
+     link : function(scope,element,attrs,ctrl,transclude)
     {
+      transclude(scope.$parent, function(clone,scope) { 
+      if(attrs.query!='query')       
+        scope.query=attrs.query;
+      });
       scope.getTemplate = function(){
-
       var showprofile= scope.showprofile.toLowerCase();
 
        if(showprofile === "true")
