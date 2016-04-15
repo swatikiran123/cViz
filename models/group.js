7ced691f51baefe4b68f5cdca8df9cc81777aea0
punampatekar1 <ppatekar@csc.com>
2016-04-15 21:37:12 +0530

    cvm issue #111 and add User to group dialog box Style

--- models/group.js
+++ models/group.js
@@ -7,8 +7,8 @@ var userSchema = require('./user');
 
 var groupSchema = new mongoose.Schema({
 
-	name 							: { type: String },
-	description						: { type: String },
+	name 							: { type: String, required: true },
+	description						: { type: String, required: true },
 	users 							: [{ type: Schema.Types.ObjectId, ref: 'User' }]
 	
 	});
