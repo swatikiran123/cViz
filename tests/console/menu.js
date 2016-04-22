a04c739a8e14b0e9316326181fba80867650f9da
sankarvema <sans.vema@gmail.com>
2016-04-22 21:34:10 +0530

    secure.isInAnyGroup fixed
    - side menu builder fixed
    
    Signed-off-by: sankarvema <sans.vema@gmail.com>

--- tests/console/menu.js
+++ tests/console/menu.js
@@ -9,26 +9,31 @@ var secure = require(constants.paths.scripts + '/secure');
 
 require(constants.paths.scripts + '/database');
 
-userService.getOneById('A02234567892345678900021')
+// var userId = "A02234567892345678900031"; //vManager
+// var userId = "A02234567892345678900025"; //client
+// var userId = "A02234567892345678900026"; //exec
+var userId = "A02234567892345678900003"; 
+
+userService.getOneById(userId)
 	.then(function(thisUser){
 		if(thisUser){
 			// call service with this user
 			logger.writeLine('',0,"User: " + thisUser.name.first + ' ' + thisUser.name.last);
 			logger.writeLine('',0,"User: " + thisUser.email);
-			logger.writeLine('',0,"member of " + thisUser.memberOf);
+			logger.writeLine('',0,"member of " + secure.getGroups(thisUser));
 
-			if( secure.isInAnyGroups(thisUser, "customer"))	{
-						logger.writeLine('' , 2,"Found customer!!!");
-					}
-					else if(secure.isInAnyGroups(thisUser, "exec")){
-								logger.writeLine('', 2,"Found exec!!!");
-					}
-					else if(secure.isInAnyGroups(thisUser, "vManager")){
-								logger.writeLine('', 2,"Found vManager!!!");
-					}
-					else if( secure.isInAnyGroups(thisUser, "user")){
-						logger.writeLine('' , 2, "Found user!!!");
-					}
+			// if( secure.isInAnyGroups(thisUser, "customer"))	{
+			// 			logger.writeLine('' , 2,"Found customer!!!");
+			// 		}
+			// 		else if(secure.isInAnyGroups(thisUser, "exec")){
+			// 					logger.writeLine('', 2,"Found exec!!!");
+			// 		}
+			// 		else if(secure.isInAnyGroups(thisUser, "vManager")){
+			// 					logger.writeLine('', 2,"Found vManager!!!");
+			// 		}
+			// 		else if( secure.isInAnyGroups(thisUser, "user")){
+			// 			logger.writeLine('' , 2, "Found user!!!");
+			// 		}
 
 			var script =  menuBuilder.getMenu(thisUser, "side");
 			console.log(script)
