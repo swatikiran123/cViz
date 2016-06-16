ce6e7307ad878372d5a4300a5fd8587db456ba06
agupta330 <agupta330@csc.com>
2016-06-16 21:18:22 +0530

    sessionDescription Issue fixed,comment issue fixed in visit,messaging/calling and download contact(txt) implemted in contact,

--- public/m/contacts/contactsCtrl.js
+++ public/m/contacts/contactsCtrl.js
@@ -2,35 +2,50 @@ angular.module('contacts')
 
 .controller('spocCtrl', function($scope, $rootScope, $location, appService) {
 
-	appService.activeVisit().then(function(avisit){
-		var str= String(avisit.locations);
-		var loc= str.split(",");
-		$location.path("contacts/"+loc[0]);
-	})
+  appService.activeVisit().then(function(avisit){
+    var str= String(avisit.locations);
+    var loc= str.split(",");
+    $location.path("contacts/"+loc[0]);
+  })
 
 })
 
-.controller('contactsCtrl', function($scope, $routeParams, $http, appService) {
-
+.controller('contactsCtrl', function($scope, $routeParams, $http, appService,$window) {
+  $scope.fileDataUrl =[];
   $http.get('/api/v1/secure/contactList/city/' +$routeParams.city,{
     cache: true
   }).success(function(response) {
     $scope.contactList = response;
+
+    console.log(response);
+    for(var i=0;i<$scope.contactList.length;i++)
+    {
+    var data = "Name:" + $scope.contactList[i].name+ " "+"Contact No: "+$scope.contactList[i].contactNo+" "+"Email: "+$scope.contactList[i].email+"     "+"Summary: "+$scope.contactList[i].summary,
+    blob = new Blob([data], { type: 'text/plain' }),
+    url = $window.URL || $window.webkitURL;
+    $scope.fileDataUrl.push(url.createObjectURL(blob));
+    }
   })
 
-	appService.activeVisit().then(function(avisit){
+  appService.activeVisit().then(function(avisit){
     var str= String(avisit.locations);
     $scope.cities = str.split(/[ ,]+/);
 
     $scope.title=avisit.title;
     $scope.anchor=avisit.anchor;
 
-   	$http.get('/api/v1/secure/admin/users/'+$scope.anchor,{
-	    cache: true
-	  }).success(function(response) {
+    $http.get('/api/v1/secure/admin/users/'+$scope.anchor,{
+      cache: true
+    }).success(function(response) {
        $scope.anchor=response;
+       $scope.anchorName = response.name.prefix + response.name.first + response.name.middle + response.name.last + response.name.suffix;
+       console.log($scope.anchor);
+       var data = "Name:" + $scope.anchor.name.first + " " + $scope.anchor.name.last+ " "+"Contact No: "+$scope.anchor.contactNo[0].contactNumber+" "+"Email: "+$scope.anchor.email+"     "+"Summary: "+$scope.anchor.summary,
+       blob = new Blob([data], { type: 'text/plain' }),
+       url = $window.URL || $window.webkitURL;
+       $scope.fileUrl = url.createObjectURL(blob);
     })
-	})
+  })
 
   $scope.collapseDiv = function(index, text){
     var ele = angular.element(document.getElementById(text + index));
@@ -42,4 +57,10 @@ angular.module('contacts')
       ele.prev().removeClass('chevron-down-arrow');
     }
   };
+
+
 })
+
+.config(['$compileProvider', function($compileProvider) {
+  $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|file|sms|blob|mailto|tel|):/);
+}]);
