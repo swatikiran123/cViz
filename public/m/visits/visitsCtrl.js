adcda8c5e6b8c27dc78151c8e3a9deb26aa78299
agupta330 <agupta330@csc.com>
2016-06-21 12:00:39 +0530

    All Visits Tab included in mobile side for admin,visit intiator

--- public/m/visits/visitsCtrl.js
+++ public/m/visits/visitsCtrl.js
@@ -21,12 +21,49 @@ angular.module('visits')
 })
 
 .controller('myVisitsCtrl', function($scope, $rootScope, $location, $http) {
+
+	$scope.myVisitsAll = {};
+	
 	$scope.setTimeline = function(time){
 		$scope.timeline = time;
 		console.log("setting timeline to " + $scope.timeline )
+
+		if($scope.timeline == "past" || $scope.timeline == "today" || $scope.timeline == "further")
+		{	
 		$scope.visitBatch = $scope.allVisits[$scope.timeline];
-		$scope.pastVisitBatch = $scope.allVisits["past"];
-		$scope.futureVisitBatch = $scope.allVisits["further"];
+		}
+		if($scope.timeline == "all")
+		{	
+		$scope.allVisitsData = [];	
+		for(var i=0;i<$scope.allVisits["past"].visits.length;i++)
+		{
+			$scope.allVisitsData.push($scope.allVisits["past"].visits[i]);
+		}
+
+		for(var k=0;k<$scope.allVisits["today"].visits.length;k++)
+		{
+			$scope.allVisitsData.push($scope.allVisits["today"].visits[k]);
+		}
+
+		for(var j=0;j<$scope.allVisits["further"].visits.length;j++)
+		{
+			$scope.allVisitsData.push($scope.allVisits["further"].visits[j]);
+		}
+		var uniqueIDs = [];
+		var uniqueArray = [];
+
+		for(var i = 0; i < $scope.allVisitsData.length; i++){
+
+			if(uniqueIDs.indexOf($scope.allVisitsData[i]._id) === -1){
+				uniqueArray.push($scope.allVisitsData[i])
+				uniqueIDs.push($scope.allVisitsData[i]._id);
+			}
+		}
+		$scope.myVisitsAll.start = $scope.allVisitsData[0].startDate;
+		$scope.myVisitsAll.end = $scope.allVisitsData[$scope.allVisitsData.length - 1].endDate;
+		$scope.myVisitsAll.visits = uniqueArray;
+		$scope.visitBatch = $scope.myVisitsAll;
+		}
 	}
 	  $scope.getSession = function(item1){
 	  	console.log('helow'+item1);
@@ -43,11 +80,38 @@ angular.module('visits')
 		cache: true
 	}).success(function(response) {
 	    $scope.allVisits = response;
+	    $scope.allVisitsData = [];	
+	    for(var i=0;i<$scope.allVisits["past"].visits.length;i++)
+	    {
+	    	$scope.allVisitsData.push($scope.allVisits["past"].visits[i]);
+	    }
+
+	    for(var k=0;k<$scope.allVisits["today"].visits.length;k++)
+	    {
+	    	$scope.allVisitsData.push($scope.allVisits["today"].visits[k]);
+	    }
+
+	    for(var j=0;j<$scope.allVisits["further"].visits.length;j++)
+	    {
+	    	$scope.allVisitsData.push($scope.allVisits["further"].visits[j]);
+	    }
+	    var uniqueIDs = [];
+	    var uniqueArray = [];
+
+	    for(var i = 0; i < $scope.allVisitsData.length; i++){
+
+	    	if(uniqueIDs.indexOf($scope.allVisitsData[i]._id) === -1){
+	    		uniqueArray.push($scope.allVisitsData[i])
+	    		uniqueIDs.push($scope.allVisitsData[i]._id);
+	    	}
+	    }
+
+	    $scope.allLength = uniqueArray.length;
 			if($scope.timeline=="" || $scope.timeline===undefined){
 				$scope.timeline = "today";
 				$scope.visitBatch = $scope.allVisits[$scope.timeline];
-				$scope.pastVisitBatch = $scope.allVisits["past"];
-				$scope.futureVisitBatch = $scope.allVisits["further"];
+				// $scope.pastVisitBatch = $scope.allVisits["past"];
+				// $scope.futureVisitBatch = $scope.allVisits["further"];
 			}
 		}
 	)
